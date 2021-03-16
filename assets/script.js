f776e3fe868170cadf503179d6c500456d215631
mjpagenkopf <78058404+mjpagenkopf@users.noreply.github.com>
2021-03-16 18:50:18 -0400

    made updates to js - now it will place the answers into the buttons and cycle through each index of the array. Still need to finalize the timer and the high score page

--- assets/script.js
+++ assets/script.js
@@ -6,7 +6,7 @@ let timerElement = document.querySelector(".timer-count");
 
 const questionText = document.getElementsByClassName("question-text")[0];
 let buttonBox = document.getElementsByClassName("button-box");
-let answerButton = document.getElementsByClassName("answer-button");
+let answerButton = document.getElementsByClassName("answer-button")[0];
 let firstChoice = document.getElementById("0"); //choice a
 let secondChoice = document.getElementById("1"); //choice b
 let thirdChoice = document.getElementById("2"); //choice c
@@ -15,6 +15,9 @@ let fourthChoice = document.getElementById("3"); //choice d
 let timer;
 let timerCount;
 let questionsIndex = 0; 
+let currentQuestion;
+let currentAnswers;
+let score = 0;
 
 //array of 5 objects    questions = ["0", "1", "2", "3", "4"]
 let questions = [
@@ -51,78 +54,100 @@ let questions = [
 ];
 
 
-
-startButton.addEventListener("click", function() {
+startButton.addEventListener("click", function() { 
   startPage.setAttribute("style", "display:none");
   questionsPage.removeAttribute("style");
   //start timer here
-  timerCount = 100;
-  startTimer();
-  populateQuestionPage(questionsIndex);
+  // timerCount = 100;
+  // startTimer();
+  populateQuestionPage(questionsIndex); //questionsIndex is 0
 })
 
-
-function populateQuestionPage(idx) {
-  let currentQuestion = questions[idx]
-  forQuestionPageArrayTitle(currentQuestion)
+function populateQuestionPage(idx) {   //idx is a placeholder as in paramters ARE PLACEHOLDERS - in this case, it is  representing questionsIndex aka 0
+  currentQuestion = questions[idx] //questions[idx] is also currently representing 0
+  currentAnswers = questions[idx].choices 
+  forQuestionPageArrayTitle(currentQuestion) //currentQuestion = to first object of questions array
+  populateAnswerButtons(currentAnswers) 
 }
 
 function forQuestionPageArrayTitle(obj) {
   questionText.textContent = obj.title
-  populateQuestionPage.textContent = obj.choices
 }
 
-function populateAnswerButtons(arr) {
+function populateAnswerButtons(arr) { //arr is equal to currentAnswers
     for (let i = 0; i < arr.length; i++)
     {
-        const thisButton = document.getElementById(`{i}`)
-        thisButton.textContent = arr[i]
-        if()
+        let thisButton = document.getElementById(`${i}`) //template literals
+        thisButton.textContent = arr[i] //questions[idx].choices->currentAnswers->arr
+        thisButton.addEventListener("click", checkAnswerAndSeeIfCorrect) //
     }
   }
 
-questionsIndex++
+  function checkAnswerAndSeeIfCorrect(e) {  //e equals the word "event"
+      const answerButtons = document.getElementsByClassName("answer-button") //not being used
+      if (e.target.textContent === questions[questionsIndex].answer) {
+        score++
+        questionsIndex++
+        populateQuestionPage(questionsIndex)
+        // alert
+      } 
+      
 
-populateQuestionPage;
+
+//will need if statement for when questionsIndex is done instead of it rotating back around to beginning of array
+
+
+
+
+
+
+      // for (let i = 0; i < answerButtons.length; i++) {
+      //   console.log(answerButtons[i].textContent)
+      //   answerbuttons[i].addEventListener("click", function(e) {
+      //   e.preventDefault
+      // })
+    }
+
+
 
 // The setTimer function starts and stops the timer and triggers winQuiz() and loseQuiz()
-function startTimer() {
-  // Sets timer
-  timer = setInterval(function() {
-    timerCount--;
-    timerElement.textContent = timerCount;
-    if (timerCount >= 0) {
-      // Tests if win condition is met
-      // if (isWin && timerCount > 0) {
-      //   // Clears interval and stops timer
-      //   clearInterval(timer);
-      //   winQuiz();
-      // }
-    }
-    // Tests if time has run out
-    if (timerCount === 0) {
-      // Clears interval
-      // clearInterval(timer);
-      loseQuiz();
-    }
-  }, 1000);
-}
+// function startTimer() {
+//   // Sets timer
+//   timer = setInterval(function() {
+//     timerCount--;
+//     timerElement.textContent = timerCount;
+//     if (timerCount >= 0) {
+//       // Tests if win condition is met
+//       // if (isWin && timerCount > 0) {
+//       //   // Clears interval and stops timer
+//       //   clearInterval(timer);
+//       //   winQuiz();
+//       // }
+//     }
+//     // Tests if time has run out
+//     if (timerCount === 0) {
+//       // Clears interval
+//       // clearInterval(timer);
+//       loseQuiz();
+//     }
+//   }, 1000);
+// }
 
 
 
-// Bonus: Add reset button
-var resetButton = document.querySelector(".reset-button");
+// // Bonus: Add reset button
+// var resetButton = document.querySelector(".reset-button");
 
-function resetQuiz() {
-// Resets win and loss counts
-winCounter = 0;
-loseCounter = 0;
-// Renders win and loss counts and sets them into client storage
-setWins()
-setLosses()
-}
-// Attaches event listener to button
-resetButton.addEventListener("click", resetQuiz)
+// function resetQuiz() {
+// // Resets win and loss counts
+// winCounter = 0;
+// loseCounter = 0;
+// // Renders win and loss counts and sets them into client storage
+// setWins()
+// setLosses()
+// }
+// // Attaches event listener to button
+// resetButton.addEventListener("click", resetQuiz)
 
 
 
@@ -248,4 +273,4 @@ resetButton.addEventListener("click", resetQuiz)
 // console.log(object["first"])
 
 // really must understand that arrays are looking at items in an Index. Assigns that item is just a number. 
-// objects are not like arrays in that sense. it isn't just an indexed number. it is the property, value and more information//
+// objects are not like arrays in that sense. it isn't just an indexed number. it is the property, value and more information
